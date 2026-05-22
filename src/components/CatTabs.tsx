9a71446953f760cc1eb3525780d185e6a67b2806
AdithyaTJ <adithza77@gmail.com>
2026-05-22 14:58:18 +0530

    added basic responsiveness to UI

--- src/components/CatTabs.tsx
+++ src/components/CatTabs.tsx
@@ -13,7 +13,7 @@ function CatTabs() {
   const categories = Object.keys(CATEGORY_MAP)
 
   return (
-    <div className='flex gap-4 overflow-x-auto scrollbar-hide scrollbar-hide py-6 md:px-12 justify-center sticky top-16 z-40 bg-black w-full'>
+    <div className='flex md:gap-3 overflow-x-scroll scrollbar-hide scrollbar-hide py-6 sm:px-12 justify-start md:justify-center sticky top-16 z-40 bg-black w-full'>
       {categories.map((category) => {
         const isActive = activeCategory === category;
 
@@ -22,7 +22,7 @@ function CatTabs() {
             key={category}
             href={`/?category=${category}`}
             className={`
-              px-5 py-2 rounded-sm text-lg font-medium whitespace-nowrap transition-all duration-200
+              px-2 md:px-4 lg:text-lg py-2 rounded-sm text-sm font-medium whitespace-nowrap transition-all duration-200
               ${
                 isActive
                   ? " text-blue-600"
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,7 +21,7 @@ function SearchBar() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for news..."
-          className=" pl-12 pr-12 py-2.5 bg-gray-900   rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:bg-gray-800 transition-all"
+          className=" pl-12 pr-12 py-2 md:py-2.5 w-56 bg-gray-900   rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent focus:bg-gray-800 transition-all"
         />
       </form>
     </div>

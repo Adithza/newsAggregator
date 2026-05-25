5f6a03abd7cb08dc011c94254f349d6d17cac053
AdithyaTJ <adithza77@gmail.com>
2026-05-25 14:38:19 +0530

    deployment bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ export default async function Home({
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <CatTabs />
+      <Suspense fallback={null}>
+        <CatTabs />
+      </Suspense>
       <Suspense key={`${category ?? "all"}-${country ?? "all"}`} fallback={<Loading />}>
         <NewsFeed articles={data.articles} nextPage={data.nextPage} />
       </Suspense>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import SearchBar from './SearchBar'
-import CatTabs from './CatTabs'
 import Link from 'next/link'
 import CountryFilter from './CountryFilter'
 
@@ -10,8 +9,17 @@ function Navbar() {
       <Link href="/" className=" text-lg md:text-2xl font-bold bg-linear-to-r from-blue-500 to-blue-700 bg-clip-text text-transparent whitespace-nowrap">
         DailyPlanet
       </Link>
-      <CountryFilter />
-      <div className='ml-auto'><SearchBar /></div>
+      <Suspense
+        fallback={
+          <div className="flex flex-1 items-center justify-end gap-4 ml-4">
+            <div className="h-7 w-40 bg-gray-800 rounded animate-pulse" />
+            <div className="h-10 w-56 bg-gray-800 rounded-lg animate-pulse" />
+          </div>
+        }
+      >
+        <CountryFilter />
+        <div className='ml-auto'><SearchBar /></div>
+      </Suspense>
     </div>
   )
 }

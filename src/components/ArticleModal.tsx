ae91af874e44589decb3f92497ebbb17ed9430fd
AdithyaTJ <adithza77@gmail.com>
2026-06-16 15:59:11 +0530

    sanitized guardian html content

--- src/components/ArticleModal.tsx
+++ src/components/ArticleModal.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -8,8 +10,11 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Article } from "@/lib/news/types"
+import DOMPurify from "isomorphic-dompurify"
 
 export function ArticleModal({ article }: {article:Article}) {
+
+  const cleanHTML =  DOMPurify.sanitize(article.content ?? "")
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -17,8 +22,8 @@ export function ArticleModal({ article }: {article:Article}) {
       </DialogTrigger>
       <DialogContent showCloseButton={true}>
         {article.source === "Guardian" ? <><DialogHeader className="bg-gray-800 p-4">
-          <DialogTitle className="px-4 text-2xl">{article.title}</DialogTitle>
-          <DialogDescription className="px-4 flex gap-4">
+          <DialogTitle className="px-4 text-xl">{article.title}</DialogTitle>
+          <DialogDescription className="px-4 flex gap-4 text-md">
             
             <span>{article.byline}</span>
             <span>{new Date(article.publishedAt).toLocaleString("en-IN")}</span>
@@ -29,7 +34,7 @@ export function ArticleModal({ article }: {article:Article}) {
         <div className="mx-4 no-scrollbar  overflow-y-auto p-4">
           
             <img src={article.thumbnail} className="mx-auto block mt-5 mb-10 w-full"></img>
-            <div className="text-xl [&_p]:mb-6" dangerouslySetInnerHTML={{ __html:article.content!}} />
+            <div className="text-lg [&_p]:mb-6" dangerouslySetInnerHTML={{ __html:cleanHTML!}} />
         </div>
         </>: <iframe src={article.url} className="w-full h-full"></iframe>
         }
--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -11,13 +11,13 @@ function NewsCard({article}: {article: any}) {
             <img src={article.thumbnail || "/image.png"} alt="Article Image" onError={(e) => e.currentTarget.src = "/image.png"} className="w-full h-full object-cover" />
         </div>
         <div className='p-5 space-y-2'>
-            <h2 className="text-l lg:text-xl font-bold line-clamp-2">{article.title}</h2>
+            <h2 className="text-md lg:text-xl font-bold line-clamp-2">{article.title}</h2>
             <div className='flex gap-2'>
               <p className="text-xs lg:text-sm text-gray-600">{new Date(article.publishedAt).toLocaleString("en-IN")}</p>
               <p className='text-xs lg:text-sm text-gray-300 pl-2'>{article.source}</p>
               {/*<p className='text-xs lg:text-sm text-gray-300 pl-2'>{article.country? article.country : ""}</p>*/}
             </div>
-            <p className='text-xs lg:text-sm line-clamp-2'>{article.content}</p>
+            <p className='text-xs lg:text-sm line-clamp-2'>{article.content?.replace(/<[^>]*>/g, "")}</p>
             <ArticleModal article={article} />
         </div>
     </div>
